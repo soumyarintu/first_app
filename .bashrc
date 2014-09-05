cfb0c08faf71ae209a32b961fb18c6bfd8896ec2
Soumya Das <soumyarintu@gmail.com>
2014-09-05 08:04:22 +0530

    Initialize Reporsitory

---
+++ .bashrc
@@ -0,0 +1,1 @@
+alias subl='"C:\Program Files\Sublime Text 2\sublime_text.exe"'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'e959be4a79ac07e763cfd53118efd357e8fd290ff70383619b77f6e6ab795983d7962da7d8814b298178a4cb61b8635d7933062abaeab6e39529b27ed5b271c9'
